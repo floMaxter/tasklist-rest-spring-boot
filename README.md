2497f88134676b8db81108971c1162d75823689f
Max Vdovin <maxpostupaet@gmail.com>
2024-08-22 12:01:41 +0300

    Integrate JWT-based authentication using Spring Security.
    - Add a database connection and migration for it.
    - Add AppConfig for configuring Spring Security context.
    - Implement JwtTokenFilter to handle JWT token validation.
    - Create JwtTokenProvider to manage token generation and validation logic.
    - Develop JwtUserDetailsService to load user-specific data during authentication.

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # tasklist-rest-spring-boot
 
+### Prerequisites:
+
+## Description
+
+## Getting started
+### 1. PostgreSQL
+A PostgreSQL is used to store information about tasks and users.
+
+Start in Docker:
+
+```shell
+docker run --name tasklist-db -p 5433:5432 -e POSTGRES_USER=tasklist -e POSTGRES_PASSWORD=tasklist -e POSTGRES_DB=tasklist postgres:16
+```
+
+> Note that the port in the database is not the default one
+
---
+++ src/main/java/com/projects/tasklist/web/security/JwtTokenProvider.java
@@ -0,0 +1,129 @@
+package com.projects.tasklist.web.security;
+
+import com.projects.tasklist.domain.exception.AccessDeniedException;
+import com.projects.tasklist.domain.user.Role;
+import com.projects.tasklist.domain.user.User;
+import com.projects.tasklist.service.UserService;
+import com.projects.tasklist.service.props.JwtProperties;
+import com.projects.tasklist.web.dto.auth.JwtResponse;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class JwtTokenProvider {
+
+    private final JwtProperties jwtProperties;
+
+    private final UserDetailsService userDetailsService;
+
+    private final UserService userService;
+
+    private SecretKey key;
+
+    @PostConstruct
+    public void init() {
+        this.key = Keys.hmacShaKeyFor(jwtProperties.getSecret().getBytes());
+    }
+
+    public String createAccessToken(Long userId, String username, Set<Role> roles) {
+        Claims claims = Jwts.claims().subject(username)
+                .add("id", userId)
+                .add("roles", resolveRoles(roles))
+                .build();
+        Date now = new Date();
+        Date validity = new Date(now.getTime() + jwtProperties.getAccess());
+        return Jwts.builder()
+                .claims(claims)
+                .issuedAt(now)
+                .expiration(validity)
+                .signWith(key)
+                .compact();
+    }
+
+    private List<String> resolveRoles(Set<Role> roles) {
+        return roles.stream()
+                .map(Enum::name)
+                .collect(Collectors.toList());
+    }
+
+    public String createRefreshToke(Long userId, String username) {
+        Claims claims = Jwts.claims().subject(username)
+                .add("id", userId)
+                .build();
+        Date now = new Date();
+        Date validity = new Date(now.getTime() + jwtProperties.getRefresh());
+        return Jwts.builder()
+                .claims(claims)
+                .issuedAt(now)
+                .expiration(validity)
+                .signWith(key)
+                .compact();
+    }
+
+    public JwtResponse refreshUserToken(String refreshToken) {
+        JwtResponse jwtResponse = new JwtResponse();
+        if (!validateToken(refreshToken)) {
+            throw new AccessDeniedException();
+        }
+        Long userId = Long.valueOf(getId(refreshToken));
+        User user = userService.getById(userId);
+        jwtResponse.setId(userId);
+        jwtResponse.setUsername(user.getUsername());
+        jwtResponse.setAccessToken(createAccessToken(userId, user.getUsername(), user.getRoles()));
+        jwtResponse.setRefreshToken(createRefreshToke(userId, user.getUsername()));
+        return jwtResponse;
+    }
+
+    public boolean validateToken(String token) {
+        Jws<Claims> claims = Jwts
+                .parser()
+                .verifyWith(key)
+                .build()
+                .parseSignedClaims(token);
+        return claims.getPayload()
+                .getExpiration()
+                .after(new Date());
+    }
+
+    private String getId(String token) {
+        return Jwts
+                .parser()
+                .verifyWith(key)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .get("id", String.class);
+    }
+
+    private String getUsername(String token) {
+        return Jwts
+                .parser()
+                .verifyWith(key)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .getSubject();
+    }
+
+    public Authentication getAuthentication(String token) {
+        String username = getUsername(token);
+        UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+        return new UsernamePasswordAuthenticationToken(userDetails, "", userDetails.getAuthorities());
+    }
+}
